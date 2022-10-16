34c9be4f1e937f4ad1e2a457f41cf4d1a140a061
Dmitry Zhuravlev <dmitryzhur@mail.ru>
2022-10-16 19:05:08 +0500

    fix: cors err

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -20,6 +20,7 @@ const cors = (req, res, next) => {
   if (allowedCors.includes(origin)) {
   // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
     res.header('Access-Control-Allow-Origin', origin); // либо  res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Credentials', true);
   }
 
   // Если это предварительный запрос, добавляем нужные заголовки
